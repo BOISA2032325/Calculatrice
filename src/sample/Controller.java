c272e36d8746eee61f706bcb6af44546588fce29
sambp <2032325@etu.cegepjonquiere.ca>
2021-11-11 09:42:34 -0500

    1

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,24 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class Controller {
+    @FXML
+    private Button buttonAdditionner;
+    @FXML
+    private Button buttonSoustraction;
+    @FXML
+    private TextField textField;
+
+    public void clicAdditonner(){
+        String premierChiffre = textField.getText();
+        textField.setText(premierChiffre+"+");
+    }
+
+    public void clicSoustraire(){
+        String chiffre = textField.getText();
+        textField.setText(chiffre+"-");
+    }
 }
