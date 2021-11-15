f4795561a6186ed9c997078002bc19d0b25ae29b
2032708 <2032708@etu.cegepjonquiere.ca>
2021-11-15 13:11:58 -0500

    test commit

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -6,11 +6,20 @@ import javafx.scene.control.TextField;
 
 public class Controller {
     @FXML
+    private TextField textField;
+    @FXML
     private Button buttonAdditionner;
     @FXML
     private Button buttonSoustraction;
     @FXML
-    private TextField textField;
+    private Button buttonMultiplication;
+    @FXML
+    private Button buttonDivision;
+    @FXML
+    private Button buttonCarre;
+    @FXML
+    private Button buttonRacine;
+
 
     public void clicAdditonner(){
         String premierChiffre = textField.getText();
@@ -20,5 +29,25 @@ public class Controller {
     public void clicSoustraire(){
         String chiffre = textField.getText();
         textField.setText(chiffre+"-");
+    }
+
+    public void clicMultiplication(){
+        String chiffre = textField.getText();
+        textField.setText(chiffre+"*");
+    }
+
+    public void clicDivision(){
+        String chiffre = textField.getText();
+        textField.setText(chiffre+"/");
+    }
+
+    public void clicCarre(){
+        String chiffre = textField.getText();
+        textField.setText(chiffre+"^2");
+    }
+
+    public void clicRacine(){
+        String chiffre = textField.getText();
+        textField.setText(chiffre+"√");
     }
 }
